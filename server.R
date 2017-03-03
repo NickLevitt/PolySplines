5f7ab13fa005df43d49d06cbedd416d246f66128
Nicholas Levitt <Levittation@Nicholas-Fancy-Computer.local>
2017-03-02 18:37:40 -0800

    Added comments

--- server.R
+++ server.R
@@ -1,33 +1,48 @@
 library(ggplot2)
 library(splines)
+
+# Initialize the server
 shinyServer(function(input, output, session) {
   
+  # Generate the UI for selecting the number of knots.
+  # Set the max number of knots possible to the number of datapoints minus one.
   output$knotNumber = renderUI({
     sliderInput("knots", "Number of Knots",
                 min = 1, max = input$datasize - 1, value = 1, step = 1)
   })
   
+  # Render the plot.
   output$splinePlot = renderPlot({
+    # If the data is selected to be randomly generated:
     if(input$data == 'random') {
-    x = c(1:input$datasize)
-    mu = 10 + 5 * sin(x*pi / 24) - 2 * cos((x-6)*4/24)
-    set.seed(2010)
-    eee = rnorm(length(mu))
-    y = mu+eee
-    linedf = data.frame(cname = seq(range(x)[1],range(x)[2],(((max(x)-min(x))-1)/input$knots))[2:(input$knots)])
-    df = data.frame(x,y)
+      # Generate the x and y data, set the seed for consistency
+      x = c(1:input$datasize)
+      mu = 10 + 5 * sin(x*pi / 24) - 2 * cos((x-6)*4/24)
+      set.seed(2010)
+      eee = rnorm(length(mu))
+      y = mu+eee
+      # Build the dataframe to store the knots in
+      linedf = data.frame(cname = seq(range(x)[1],range(x)[2],(((max(x)-min(x))-1)/input$knots))[2:(input$knots)])
+      # Build the dataframe to store the x,y data in
+      df = data.frame(x,y)
     }
+    # If the data is selected to be the mtcars
     else if (input$data == 'cars') {
+      # Grab the data from mtcars, x is quarter mile time and y is weight
       x = mtcars$qsec
       y = mtcars$wt
+      # Build the dataframe to store the knots in
       linedf = data.frame(cname = seq(range(x)[1],range(x)[2],(((max(x)-min(x))-1)/input$knots))[2:(input$knots)])
+      # Build the dataframe to store the x,y data in
       df = data.frame(x,y)
     }
     
+    # If the number of knots is not equal to one, display the data and the knots on a ggplot graph.
     if(input$knots != 1) {
     qplot(x, y, data=df, geom=c('point', 'smooth'), method='lm', se = FALSE, formula=y ~ bs(x, knots = seq(range(x)[1],range(x)[2],(((max(x)-min(x))-1)/input$knots))[2:(input$knots)], degree = input$degrees)) +
       geom_vline(data = linedf, aes(xintercept= cname, color = 'red'), linetype = 'dashed', show_guide = F)
     }
+    # Otherwise, just display the data and plot the line/polynomial
     else{
     qplot(x, y, data=df, geom=c('point', 'smooth'), method='lm', se = FALSE, formula=y ~ bs(x, df = input$knots - 1, degree = input$degrees))
     }
--- ui.R
+++ ui.R
@@ -1,23 +1,24 @@
 # Written by Alex Jacobs and Nick Levitt
 library(ggplot2)
 library(splines)
+
 # Define UI for spline visualization application
 shinyUI(navbarPage("Visualization of Polynomial Splines", 
                    tabPanel("Spline",
                             sidebarLayout(
                               sidebarPanel(
-                                selectInput('data', "Select Data To Use",
+                                selectInput('data', "Select Data To Use",  # Select between mtcars and randomly generated data
                                             choices = c('cars','random')),
-                                sliderInput("datasize", "Number of Data Points",
+                                sliderInput("datasize", "Number of Data Points",  # Select number of datapoints to be shown/generated
                                             min = 10, max = 200, value = 50, step = 10),
-                                uiOutput('knotNumber'),
-                                sliderInput('degrees', 'Degrees of Spline',
+                                uiOutput('knotNumber'),  # Select the number of knots to render
+                                sliderInput('degrees', 'Degrees of Spline',  # Select the degree of the spline to render
                                             min = 1, max = 20, value = 1, step = 1)
                               ),
 
                               #Render the plots
                               mainPanel(
-                                plotOutput('splinePlot')
+                                plotOutput('splinePlot')  # Display the rendered graph
                               )
                             )
                    )
